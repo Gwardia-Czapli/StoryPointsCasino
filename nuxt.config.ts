c2781f6408b763599db0b7788c73c4f5b5832ab3
norbiros <norbiros@protonmail.com>
2025-03-09 20:50:34 +0100

    feat: Pre-initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: '2025-03-09',
+
+  // Module related configuration
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+})
