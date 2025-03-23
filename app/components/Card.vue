40341d4f3d1c72bf653d24ec772486103204db3b
wojpo <ilikeahottodogu@gmail.com>
2025-03-23 01:24:45 +0100

    feat(frontend): card component

---
+++ app/components/Card.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  content: string
+}>()
+</script>
+
+<template>
+  <div class="bg-black/40 border-2 rounded-lg text-center max-w-125 p-8">
+    <p class="text-2xl font-bold text-[#32BE42]">
+      {{ title }}
+    </p>
+    <p class="text-base mt-3">
+      {{ content }}
+    </p>
+  </div>
+</template>
