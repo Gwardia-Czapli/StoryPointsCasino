ec07f12451b6b8cdb13d3e384ff8f23ad7e4c47a
wojpo <ilikeahottodogu@gmail.com>
2025-03-23 23:36:13 +0100

    feat(frontend): Refactor styles and add logging option

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,14 @@
+<script setup lang="ts">
+const { loggedIn } = useUserSession()
+</script>
+
 <template>
   <div class="relative h-screen">
-    <div class="absolute inset-0 md:bg-[url(/img/casino-chips-bg.jpg)] md:before:bg-black/50 bg-cover bg-center before:absolute before:inset-0" />
+    <div class="absolute inset-0 md:bg-[url(/img/casino-chips-bg.webp)] md:before:bg-black/50 bg-cover bg-center before:absolute before:inset-0" />
     <div class="relative z-10 flex flex-col items-center justify-center">
       <div class="mt-30 text-center">
         <p class="text-5xl md:text-7xl font-bold">
-          Story Point <br><span class="text-[#32BE42]">
+          Story Point <br><span class="text-green-600">
             Casino
           </span>
         </p>
@@ -12,22 +16,43 @@
           Let's <span class="font-bold">gamble</span> your contributions
         </p>
       </div>
-      <div class="flex max-md:flex-col gap-15 justify-center items-center mt-20">
+      <div class="mt-10 text-center">
+        <UButton
+          v-if="!loggedIn"
+          to="/api/auth/github"
+          icon="i-simple-icons-github"
+          label="Login with GitHub"
+          size="xs"
+          external
+        />
+
+        <div v-else>
+          <h2>You are logged in!</h2>
+          <NuxtLink to="/panel" class="underline">
+            Open panel here
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="flex max-md:flex-col gap-15 justify-center items-center mt-15 ">
         <Card
+          class="m-5"
           title="Contribute and earn!"
-          content="Earn story points by contributing to projects on GitHub.
-            Commit your code, open issues, make pull requests,
-            insult people in code reviews, and much more.
-            For every action on GitHub, you will earn story points based on your impact.
-            Start coding right now and gain the most valuable currency in the world."
-        />
+        >
+          Earn story points by contributing to projects on GitHub.
+          Commit your code, open issues, make pull requests,
+          insult people in code reviews, and much more.
+          For every action on GitHub, you will earn story points based on your impact.
+          Start coding right now and gain the most valuable currency in the world.
+        </Card>
         <Card
+          class="m-5"
           title="Gamble and multiply!"
-          content="Use your story points to gamble in our casino by playing exciting games!
-            Can you truly live life to the fullest if you're too scared to be a real hustler?
-            Don't be afraid of the potential to multiply your wealth.
-            Remember, you can only lose 100% of your assets, but you could gain up to 2000% or even more!"
-        />
+        >
+          Use your story points to gamble in our casino by playing exciting games!
+          Can you truly live life to the fullest if you're too scared to be a real hustler?
+          Don't be afraid of the potential to multiply your wealth.
+          Remember, you can only lose 100% of your assets, but you could gain up to 2000% or even more!
+        </Card>
       </div>
     </div>
   </div>
