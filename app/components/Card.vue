4151004e0322e14b2600904363b34f1780874c13
wojpo <ilikeahottodogu@gmail.com>
2025-03-23 23:36:32 +0100

    feat(frontend): Refactor card component

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
 defineProps<{
   title: string
-  content: string
 }>()
 </script>
 
 <template>
   <div class="bg-black/40 border-2 rounded-lg text-center max-w-125 p-8">
-    <p class="text-2xl font-bold text-[#32BE42]">
+    <h1 class="text-2xl font-bold text-green-600">
       {{ title }}
-    </p>
+    </h1>
     <p class="text-base mt-3">
-      {{ content }}
+      <slot />
     </p>
   </div>
 </template>
